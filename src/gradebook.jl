f1cc7fdfcccabee52fba2a30bf85377e24b2cfab
Joby M. Anthony III <62029937+jmanthony3@users.noreply.github.com>
2026-01-08 17:02:48 -0500

    misc effort

--- src/gradebook.jl
+++ src/gradebook.jl
@@ -4,22 +4,31 @@ using DataFrames
 
 abstract type AbstractGradebook <: AbstractDataFrame end
 struct Gradebook{T<:Union{Class,Student}} <: AbstractGradebook
+    who::T
     data::DataFrame
     assignments::Vector{Assignment}
-    who::T
     # Gradebook{T}(who, assignments, df) where {T<:Union{Class,Student}} = new{T}(who, assignments, df)
 end
 # Gradebook{Class}(class::Class, assignments::Vector{Assignment}) = new{Class}(class, assignments, DataFrame(zeros((length(class.roster), length(assignments))), [a.name for a in assignments]))
-function Gradebook{Class}(class::Class, assignments)
-    names = [a.name for a in assignments]
+function Gradebook{Class}(class::Class, assignments::Vector{Assignment})
+    names = [string(a.codename) for a in assignments]
     if any(isa.(assignments, AbstractAssignment{Group}))
         pushfirst!(names, ["ID", "Preferred", "Last", "Team", "Email"])
     else
         pushfirst!(names, ["ID", "Preferred", "Last", "Email"])
     end
-    Gradebook{Class}(class, DataFrame(zeros((length(class.roster), length(names))), names))
+    return Gradebook{Class}(class, DataFrame(zeros((length(class.roster), length(names))), names))
 end
-Gradebook{Student}(student::Student, assignments) = Gradebook{Student}(student, DataFrame(zeros((1, length(assignments))), [a.name for a in assignments]))
+function Gradebook{Student}(student::Student, assignments::Vector{Assignment})
+    names = [string(a.codename) for a in assignments]
+    if any(isa.(assignments, AbstractAssignment{Group}))
+        pushfirst!(names, ["ID", "Preferred", "Last", "Team", "Email"])
+    else
+        pushfirst!(names, ["ID", "Preferred", "Last", "Email"])
+    end
+    return Gradebook{Student}(student, DataFrame(zeros((1, length(names))), names))
+end
+
 const StudentGradebook = Gradebook{Student}
 const ClassGradebook = Gradebook{Class}
 # const Attendance{T} = Gradebook{T}
