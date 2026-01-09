fa961e8a33423c79d6ffdfd04ed734386ce3a781
Joby M. Anthony III <62029937+jmanthony3@users.noreply.github.com>
2026-01-09 10:31:45 -0500

    simplify outer construction to Union and operate within based on type

--- src/gradebook.jl
+++ src/gradebook.jl
@@ -10,23 +10,14 @@ struct Gradebook{T<:Union{Class,Student}} <: AbstractGradebook
     # Gradebook{T}(who, assignments, df) where {T<:Union{Class,Student}} = new{T}(who, assignments, df)
 end
 # Gradebook{Class}(class::Class, assignments::Vector{Assignment}) = new{Class}(class, assignments, DataFrame(zeros((length(class.roster), length(assignments))), [a.name for a in assignments]))
-function Gradebook{Class}(class::Class, assignments::Vector{Assignment})
+function Gradebook(who::T, assignments::Vector{Assignment}) where {T<:Union{Class,Student}}
     names = [string(a.codename) for a in assignments]
     if any(isa.(assignments, AbstractAssignment{Group}))
         pushfirst!(names, ["ID", "Preferred", "Last", "Team", "Email"])
     else
         pushfirst!(names, ["ID", "Preferred", "Last", "Email"])
     end
-    return Gradebook{Class}(class, DataFrame(zeros((length(class.roster), length(names))), names))
-end
-function Gradebook{Student}(student::Student, assignments::Vector{Assignment})
-    names = [string(a.codename) for a in assignments]
-    if any(isa.(assignments, AbstractAssignment{Group}))
-        pushfirst!(names, ["ID", "Preferred", "Last", "Team", "Email"])
-    else
-        pushfirst!(names, ["ID", "Preferred", "Last", "Email"])
-    end
-    return Gradebook{Student}(student, DataFrame(zeros((1, length(names))), names))
+    return Gradebook{T}(who, DataFrame(zeros(isa(who, Class) ? length(who.roster) : 1, length(names)), names), assignments)
 end
 
 const StudentGradebook = Gradebook{Student}
