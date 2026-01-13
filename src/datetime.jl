2460e622df04810b0ce33e9fa61c63fbdfe81f52
Joby M. Anthony III <62029937+jmanthony3@users.noreply.github.com>
2026-01-13 16:12:11 -0500

    stabilize for whether assignment frequency is codename or attempt parse of only supplied date

--- src/datetime.jl
+++ src/datetime.jl
@@ -1,10 +1,12 @@
 export MIDNIGHT, safe_datetime_stamp
 export dayname_codes, dayabbr_codes, dayname2codename
-export parse_time, parse_datetime
+export parse_time, parse_date, parse_datetime
 
 using Dates
 
 const MIDNIGHT = Time(23, 59, 59, 999)
+const MWF = :MWF
+const TR = :TR
 
 safe_datetime_stamp(dt::DateTime)   = replace(string(dt), "-"=>"", ":"=>"", "."=>"")
 safe_datetime_stamp()               = safe_datetime_stamp(now())
@@ -23,15 +25,14 @@ function dayname2codename(s::String)
     conjuctions = ["for", "and", "nor", "but", "or", "yet", "so"]
     prepositions = ["of", "in", "for", "with", "on", "at", "from", "into", "during", "through", "without", "under", "over", "above", "below", "to"]
     forbidden = vcat(articles, conjuctions, prepositions)
-    tokens = uppercasefirst.(lowercase.(filter(s->lowercase(s) .∉ forbidden, split(filter(!ispunct, s), " "))))
-    dayname_codes_str = string.(keys(dayname_codes))
-    return mapreduce(t->dayname_codes[findfirst(occursin(t, dayname_codes_str))], *, tokens)
+    tokens = uppercasefirst.(lowercase.(filter(s->lowercase(s) ∉ forbidden, split(filter(!ispunct, s), " "))))
+    # tokens = uppercasefirst.(lowercase.(filter(s->any(.!(occursin.(lowercase(s), forbidden))), split(filter(!ispunct, s), " "))))
+    dayname_codes_str = [string.(keys(dayname_codes))...]
+    return uppercase2symbol(mapreduce(t->"$(dayname_codes[findfirst(occursin.(t, dayname_codes_str))])", *, tokens))
 end
-function dayname2codename(s::Vector{String})
-    return mapreduce(dayname2codename, *, s)
-end
-dayname2codename(s::Symbol) = dayname2codename("$s")
-dayname2codename(s::Vector{Symbol}) = dayname2codename(map(x->"$x", s))
+dayname2codename(s::Vector{String}) = uppercase2symbol(mapreduce(x->"$(dayname2codename(x))", *, s))
+dayname2codename(s::Symbol) = uppercase2symbol(s == MWF || s == TR ? s : dayname2codename("$s"))
+dayname2codename(s::Vector{Symbol}) = uppercase2symbol(mapreduce(x->"$(dayname2codename(x))", *, s))
 
 function parse_time(t)
     if isa(t, Time)
@@ -73,6 +74,53 @@ function parse_time(t)
     end
 end
 
+function parse_date(d)
+    if isa(d, Date)
+        d = if year(d) == 1
+            Date(year(now()), month(d), day(d))
+        else
+            d
+        end
+        return d
+    elseif isa(d, String)
+        try
+            return Date(d, ISODateFormat)
+        catch
+            try
+                function parse_date_g(df)
+                    return try
+                        Date(d, df)
+                    catch exc
+                        nothing
+                    end
+                end
+                date_variations = ["y-m-d", "m-d", "yyyymmdd", "m/d/y", "m/d", "U d, y", "U d", "u. d, y", "u. d", "u d, y", "u d"]
+                dateformats = DateFormat.(date_variations)
+                i, parse, n = 0, nothing, length(dateformats)
+                while isnothing(parse)
+                    i += 1
+                    if i == n + 1
+                        return nothing
+                    end
+                    parse = parse_date_g(dateformats[i])
+                end
+                d = parse
+                d = if year(d) == 1
+                    Date(year(now()), month(d), day(d))
+                else
+                    d
+                end
+                return d
+            catch
+                @error "I could not parse the datetime..." d
+            end
+        end
+    else
+        @info "Must be `Date` or `String` but is" typeof(d)
+        @error "I could not parse the datetime..." d
+    end
+end
+
 function parse_datetime(d)
     if isa(d, DateTime)
         return d
@@ -85,10 +133,14 @@ function parse_datetime(d)
         return DateTime(d, MIDNIGHT)
     elseif isa(d, String)
         try
-            return DateTime(d, ISODateTimeFormat)
+            return DateTime(parse_date(d), MIDNIGHT)
         catch
             try
-                return DateTime(Date(DateTime(d, ISODateFormat)), MIDNIGHT)
+                try
+                    return DateTime(Date(d, ISODateFormat), MIDNIGHT)
+                catch
+                    return DateTime(d, ISODateTimeFormat)
+                end
             catch
                 try
                     function parse_datetime_g(df)
